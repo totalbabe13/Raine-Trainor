6c3a19641d0305df59c006d4dd62987f921385e9
leonardo Kaplan <kaplanleonardo@gmail.com>
2020-10-24 13:18:09 -0500

    adding js script for modal function

--- next_update/art_direction/art_dir_projects/art_direction.js
+++ next_update/art_direction/art_dir_projects/art_direction.js
@@ -1,11 +1,29 @@
 alert("sup binches")
 
-let ximages = document.querySelectorAll("img");
+let xImages = document.querySelectorAll("img");
 let modal = document.querySelector(".modal")
+let imageSources = []
+
+function testFunction() {
+  console.log("testing on click")
+  
+//   imgObj.style.border = "3px solid lime";
+} 
+
 
 // 1. Loop through ximages collection and select each image element and its source
 // 2. add onclick event listener to image elements 
 
+for (i = 0; i < xImages.length; i++) {
+    xImages[i].onclick = function(){
+        console.log('test?')
+        let srcTest = this.getAttribute('src')
+        //  console.log(srcTest)
+        modal.style.display = "block"        
+    
+        }
+    }
+// xImages[i].style.border = "3px solid lime";
 // 3. Write OnCLICK function: 
 //    When onclick: 
 //    a.   Modal display on
@@ -13,4 +31,3 @@ let modal = document.querySelector(".modal")
 
 // 4.   Add event listener to modal to toggle modal display
 
-console.log(modal)
