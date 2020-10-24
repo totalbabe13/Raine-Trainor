b202c108a6cc53a2062446b790fcc785d0de86c5
leonardo Kaplan <kaplanleonardo@gmail.com>
2020-10-23 23:16:08 -0500

    adding JS file and psuedo code for modal script

--- next_update/art_direction/art_dir_projects/art_direction.js
+++ next_update/art_direction/art_dir_projects/art_direction.js
@@ -1,5 +1,16 @@
 alert("sup binches")
 
-let ximages = document.querySelectorAll("img")
+let ximages = document.querySelectorAll("img");
+let modal = document.querySelector(".modal")
 
-console.log(ximages)
+// 1. Loop through ximages collection and select each image element and its source
+// 2. add onclick event listener to image elements 
+
+// 3. Write OnCLICK function: 
+//    When onclick: 
+//    a.   Modal display on
+//    b.   Insert child element(image) FULL SCREEN
+
+// 4.   Add event listener to modal to toggle modal display
+
+console.log(modal)
