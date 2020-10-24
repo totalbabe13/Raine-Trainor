1e488825399d591b77bb303cc268c8743986bda3
leonardo Kaplan <kaplanleonardo@gmail.com>
2020-10-24 14:49:01 -0500

    finished writing script for art direction page modals

--- next_update/art_direction/art_dir_projects/art_direction.js
+++ next_update/art_direction/art_dir_projects/art_direction.js
@@ -1,33 +1,24 @@
-alert("sup binches")
-
+// alert("sup binches")
 let xImages = document.querySelectorAll("img");
-let modal = document.querySelector(".modal")
-let imageSources = []
 
-function testFunction() {
-  console.log("testing on click")
-  
-//   imgObj.style.border = "3px solid lime";
-} 
+function removeModal() {
+    let modal = document.querySelector(".modal");
+    modal.innerHTML = '';
+    modal.style.display = "none";
+}
 
-
-// 1. Loop through ximages collection and select each image element and its source
-// 2. add onclick event listener to image elements 
+function displayModal(imgSrc) {
+    let modal = document.querySelector(".modal");
+    let imgNode = document.createElement("img");
+    modal.onclick = function() {removeModal()};
+    imgNode.className = "modal-image";
+    imgNode.src = imgSrc; 
+    modal.style.display = "grid";
+    modal.appendChild(imgNode);  
+}
 
 for (i = 0; i < xImages.length; i++) {
-    xImages[i].onclick = function(){
-        console.log('test?')
-        let srcTest = this.getAttribute('src')
-        //  console.log(srcTest)
-        modal.style.display = "block"        
-    
-        }
-    }
-// xImages[i].style.border = "3px solid lime";
-// 3. Write OnCLICK function: 
-//    When onclick: 
-//    a.   Modal display on
-//    b.   Insert child element(image) FULL SCREEN
+    xImages[i].onclick =  function() { displayModal(this.getAttribute('src'))};
+}
 
-// 4.   Add event listener to modal to toggle modal display
 
